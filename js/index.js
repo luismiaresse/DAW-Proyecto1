732c65ee804b99442fb4898f1de9064a45d31228
alejandro-aybar <alejandroaybarcifuentes@gmail.com>
2023-03-24 10:54:22 +0100

    reestructuracion index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 
-// Script para mostrar o esconder el menú al pasar el ratón por el botón
+// ---------------------------------------------------------------
+// MOSTRAR MENÚ
+// Evento: mouseover
+// DOM: getElementById(), addEventListener(), element.style
+// ---------------------------------------------------------------
+
+
 function mostrarLinks() {
     var lista = document.getElementById("dropdown");
     if (lista.style.display === "none") {
         lista.style.display = "block";
     }
 }
+
+var dropbtn = document.getElementById("dropbtn");
+dropbtn.addEventListener("mouseover", mostrarLinks);
+
+
+// ---------------------------------------------------------------
+// OCULTAR MENÚ
+// Evento: mouseout
+// DOM: getElementById(), addEventListener(), element.style
+// ---------------------------------------------------------------
 
 function esconderLinks() {
     var lista = document.getElementById("dropdown");
@@ -14,6 +30,29 @@ function esconderLinks() {
     }
 }
 
-var dropbtn = document.getElementById("dropbtn");
-dropbtn.addEventListener("mouseover", mostrarLinks);
 dropbtn.addEventListener("mouseout", esconderLinks);
+
+
+// ---------------------------------------------------------------
+// BOTON MODO OSCURO
+// Evento: click
+// DOM: getElementsByClassName(), element.innerText
+// ---------------------------------------------------------------
+
+var boton = document.getElementsByClassName("boton")[0];
+
+//document.getElementsByClassName("boton")[0].addEventListener("click", function(){
+boton.addEventListener("click", function(){
+  console.log("click")
+
+  if (this.innerText == "Modo oscuro"){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"
+    this.innerText = "Modo claro";
+  }
+  else{
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    this.innerText = "Modo oscuro";
+  }
+});
