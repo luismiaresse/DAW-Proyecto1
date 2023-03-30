510f0b6d12ad82d1c3b892492775f85ea033609d
luismi <luimiaresse03@gmail.com>
2023-03-30 20:38:46 +0200

    Regex y footer

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 
+
 // ---------------------------------------------------------------
 // MOSTRAR MENÚ
 // Evento: mouseover
 // DOM: getElementById(), addEventListener(), element.style
 // ---------------------------------------------------------------
 
-
 function mostrarLinks() {
   var lista = document.getElementById("dropdown");
   if (lista.style.display === "none") {
-      lista.style.display = "block";
+    lista.style.display = "block";
   }
 }
 
-var dropbtn = document.getElementById("dropbtn");
-dropbtn.addEventListener("mouseover", mostrarLinks);
+let dropbtn = document.getElementById("dropbtn");
+
+if (dropbtn != null) {
+  dropbtn.addEventListener("mouseover", mostrarLinks);
+}
 
 
 // ---------------------------------------------------------------
@@ -26,12 +29,13 @@ dropbtn.addEventListener("mouseover", mostrarLinks);
 function esconderLinks() {
   var lista = document.getElementById("dropdown");
   if (lista.style.display === "block") {
-      lista.style.display = "none";
+    lista.style.display = "none";
   }
 }
 
-dropbtn.addEventListener("mouseout", esconderLinks);
-
+if (dropbtn != null) {
+  dropbtn.addEventListener("mouseout", esconderLinks);
+}
 
 // ---------------------------------------------------------------
 // BOTON MODO OSCURO
@@ -39,13 +43,13 @@ dropbtn.addEventListener("mouseout", esconderLinks);
 // DOM: getElementsByClassName(), element.innerText
 // ---------------------------------------------------------------
 
-var botonTema = document.getElementsByClassName("boton-tema")[0];
+let textoClaro = "\u{2600} Modo claro";
+let textoOscuro = "\u{263D} Modo oscuro";
+let botonTema = document.getElementsByClassName("boton-tema")[0];
 
 botonTema.addEventListener("click", function(){
-  console.log("click")
-  textoClaro = "\u{2600} Modo claro";
-  textoOscuro = "\u{263D} Modo oscuro";
-  if (this.innerText == textoOscuro){
+  console.log("Cambio de tema");
+  if (this.innerText == textoOscuro) {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white"
     this.innerText = textoClaro;
@@ -57,7 +61,71 @@ botonTema.addEventListener("click", function(){
   }
 });
 
-// JQUERY
-// import $ from 'jquery';
-// $(document).ready(function(){
-// });
+
+// ---------------------------------------------------------------
+// BOTON MOSTRAR MÁS SITIOS
+// Evento: click
+
+let botonCargarSitios = document.getElementById("boton-cargar-sitios");
+
+function cargarMasSitios() {
+  let lista = document.getElementsByClassName("sitios-ocultos")[0];
+  let primero = document.createElement("div");
+  primero.classList.add("col-sm");
+  primero.innerHTML = "<figure><h3>La Galiciana</h3><p></p><img src='../img/santiago/la-galiciana.jpg' alt='La Galiciana' id='imagen-sitio'></figure>";
+  let segundo = document.createElement("div");
+  segundo.classList.add("col-sm");
+  segundo.innerHTML = "<figure><h3>Pepa a Loba (Santiago)</h3><p>Este céntrico restaurante en la zona vieja de Santiago te embaucará con sus sabores</p><img src='../img/santiago/pepa_a_loba.jpg' alt='Pepa a Loba (Santiago)'></figure>";
+  let tercero = document.createElement("div");
+  tercero.classList.add("col-sm");
+  tercero.innerHTML = "<figure><h3>La Galiciana</h3><img src='../img/santiago/la-galiciana.jpg' alt='La Galiciana' id='imagen-sitio'></figure>";
+  lista.appendChild(primero);
+  lista.appendChild(segundo);
+  lista.appendChild(tercero);
+  lista.style.visibility = "visible";
+  botonCargarSitios.style.visibility = "hidden";
+}
+
+if (botonCargarSitios != null) {
+  botonCargarSitios.addEventListener("click", cargarMasSitios);
+}
+
+
+// ---------------------------------------------------------------
+// REGEX PARA VALIDAR FORMULARIO DE COMENTARIOS
+
+let nombre = $("#nombre");
+let correo = $("#email");
+let comentario = $("#comentario");
+
+$("#boton-enviar-comentario").click(function() {
+  let errorText = "";
+  let errorId = 0;
+  let errorStyle = {
+    "color": "red",
+    "visibility": "visible",
+    "margin-top": "16px"
+  }
+
+  var regex = /\S+@\S+\.\S+/;   // Cadenas con caracter(es)@caracter(es).caracter(es)
+  if (!regex.test(correo.val())) {
+    errorText = "El correo no es válido."
+    errorId = 2;
+  }
+  regex = /\S+/;   // Cadenas con al menos un caracter
+  if (!regex.test(comentario.val())) {
+    errorText = "El comentario no es válido."
+    errorId = 3;
+  }
+  if (!regex.test(nombre.val())) {
+    errorText = "El nombre no es válido."
+    errorId = 1;
+  }
+  
+  if (errorId != 0) {
+    $("#error-comentarios").text(errorText);
+    $("#error-comentarios").css(errorStyle);
+  } else {
+    $("#error-comentarios").css("visibility", "hidden");
+  }
+});
